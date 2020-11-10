2ff4c11ebebd108e8ac4cb09532eec6d50c7a5ac
ControlledChaos <greg@ccdzine.com>
2020-11-09 23:03:21 -0800

    New website help post type.
    Hidden from menu, may not use.

--- includes/post-types-taxes/class-register-post-types.php
+++ includes/post-types-taxes/class-register-post-types.php
@@ -90,7 +90,7 @@ final class Post_Types_Register {
 		$options = [
 			'label'               => __( 'Projects', 'grande-design' ),
 			'labels'              => $labels,
-			'description'         => __( 'Custom post type description.', 'grande-design' ),
+			'description'         => __( 'Projects featured in the Grande Design portfolio.', 'grande-design' ),
 			'public'              => true,
 			'publicly_queryable'  => true,
 			'show_ui'             => true,
@@ -129,6 +129,82 @@ final class Post_Types_Register {
 		 */
 		register_post_type(
 			'project',
+			$options
+		);
+
+		/**
+		 * Post Type: Help
+		 */
+
+		$labels = [
+			'name'                  => __( 'Website Help', 'grande-design' ),
+			'singular_name'         => __( 'Help Page', 'grande-design' ),
+			'menu_name'             => __( 'Website Help', 'grande-design' ),
+			'all_items'             => __( 'All Help Pages', 'grande-design' ),
+			'add_new'               => __( 'New Help Page', 'grande-design' ),
+			'add_new_item'          => __( 'New Help Page', 'grande-design' ),
+			'edit_item'             => __( 'Edit Help Page', 'grande-design' ),
+			'new_item'              => __( 'New Help Page', 'grande-design' ),
+			'view_item'             => __( 'View Help Page', 'grande-design' ),
+			'view_items'            => __( 'View Help Page', 'grande-design' ),
+			'search_items'          => __( 'Search Help Pages', 'grande-design' ),
+			'not_found'             => __( 'No Help Pages Found', 'grande-design' ),
+			'not_found_in_trash'    => __( 'No Help Pages Found in Trash', 'grande-design' ),
+			'parent_item_colon'     => __( 'Parent Help Page', 'grande-design' ),
+			'featured_image'        => __( 'Featured image for this help', 'grande-design' ),
+			'set_featured_image'    => __( 'Set featured image for this help page', 'grande-design' ),
+			'remove_featured_image' => __( 'Remove featured image for this help page', 'grande-design' ),
+			'use_featured_image'    => __( 'Use as featured image for this help page', 'grande-design' ),
+			'archives'              => __( 'Help Pages Archives', 'grande-design' ),
+			'insert_into_item'      => __( 'Insert into Help Page', 'grande-design' ),
+			'uploaded_to_this_item' => __( 'Uploaded to this Help Page', 'grande-design' ),
+			'filter_items_list'     => __( 'Filter Help Page', 'grande-design' ),
+			'items_list_navigation' => __( 'Help Pages list navigation', 'grande-design' ),
+			'items_list'            => __( 'Help Pages List', 'grande-design' ),
+			'attributes'            => __( 'Help Page Attributes', 'grande-design' ),
+			'parent_item_colon'     => __( 'Parent Help Page', 'grande-design' ),
+		];
+
+		// Apply a filter to labels for customization.
+		$labels = apply_filters( 'help_labels', $labels );
+
+		$options = [
+			'label'               => __( 'Help Pages', 'grande-design' ),
+			'labels'              => $labels,
+			'description'         => __( 'Website user help pages.', 'grande-design' ),
+			'public'              => true,
+			'publicly_queryable'  => true,
+			'show_ui'             => true,
+			'show_in_rest'        => false,
+			'rest_base'           => '',
+			'has_archive'         => false,
+			'show_in_menu'        => false,
+			'exclude_from_search' => true,
+			// Sets user role levels, accepts array: 'capabilities'        => [],
+			'capability_type'     => 'post',
+			'map_meta_cap'        => true,
+			'hierarchical'        => false,
+			'rewrite'             => false,
+			'query_var'           => 'help',
+			'menu_position'       => 100,
+			'menu_icon'           => 'dashicons-welcome-learn-more',
+			'supports'            => [
+				'title',
+				'editor',
+				'thumbnail',
+				'excerpt'
+			],
+			'taxonomies'          => [],
+		];
+
+		// Apply a filter to arguments for customization.
+		$options = apply_filters( 'help_args', $options );
+
+		/**
+		 * Register the post type
+		 */
+		register_post_type(
+			'website_help',
 			$options
 		);
 
